85a404c1e481449f9b2dcb5ba27e26728e31a298
caiman <shimyshimya@mail.ru>
2023-01-14 15:17:36 +0500

    skills

--- web_profession/urls.py
+++ web_profession/urls.py
@@ -5,5 +5,6 @@ import web_profession.views
 urlpatterns = [
     path('', web_profession.views.MainView.as_view(), name='main'),
     path('demand/', web_profession.views.DemandView.as_view(), name='demand'),
-    path('geography', web_profession.views.GeographyView.as_view(), name='geo')
+    path('geography/', web_profession.views.GeographyView.as_view(), name='geo'),
+    path('skills/', web_profession.views.SkillsView.as_view(), name='skills'),
 ]
--- web_profession/views.py
+++ web_profession/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 
-from web_profession.models import AboutParagraph, YearStats, ImageStat, CitySal, CityFract
+from web_profession.models import AboutParagraph, YearStats, ImageStat, CitySal, CityFract, Skill
 
 
 # Create your views here.
@@ -18,7 +18,7 @@ class DemandView(TemplateView):
 
     def get_context_data(self, **kwargs):
         return {
-            "rows": YearStats.objects.all(),
+            "rows": YearStats.objects.all().order_by('year'),
             "salary": ImageStat.objects.get(name='year_salary'),
             "count": ImageStat.objects.get(name='year_count')
         }
@@ -30,8 +30,22 @@ class GeographyView(TemplateView):
 
     def get_context_data(self, **kwargs):
         return {
-            "rows_sal": CitySal.objects.all(),
-            "rows_fr": CityFract.objects.all(),
+            "rows_sal": CitySal.objects.all().order_by('-salary'),
+            "rows_fr": CityFract.objects.all().order_by('-fraction'),
             "salary": ImageStat.objects.get(name="geo_salary"),
             "fraction": ImageStat.objects.get(name="geo_fract"),
         }
+
+
+class SkillsView(TemplateView):
+
+    template_name = 'skills.html'
+
+    def get_context_data(self, **kwargs):
+        years = Skill.objects.values('year').distinct().order_by('year')
+        ctx = {}
+
+        for y in years:
+            ctx[y['year']] = Skill.objects.filter(year=y['year']).order_by('-count')
+
+        return {'data': ctx}
